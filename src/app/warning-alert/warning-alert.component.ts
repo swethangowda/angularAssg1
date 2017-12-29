4066fe6412bd71ea6edd2103aa59b80e40afea05
swethangowda <swethaiisc@gmail.com>
2017-12-29 11:15:10 +0530

    Angular Assignment 1

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `<p>Warning</p>`,
+  styleUrls: ['./warning-alert.component.css']
+
+})
+export class WarningAlertComponent {
+
+}
